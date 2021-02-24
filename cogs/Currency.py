bc108d59946a86dd5b93e1f8f5163ee03d8b6040
Jacob <56100195+jbsparrow@users.noreply.github.com>
2021-02-24 08:58:31 -0500

    Add files via upload

---
+++ cogs/Currency.py
@@ -0,0 +1,109 @@
+import datetime
+from discord.ext import commands
+import discord
+import json
+import random
+intents = discord.Intents.default()
+intents.members = True
+
+
+client = discord.Client(intents=intents)
+currenttime = datetime.datetime.now()
+guild = discord.guild
+amounts = {}
+
+
+def _save():
+    with open('amounts.json', 'w+') as f:
+        json.dump(amounts, f)
+
+
+class Currency(commands.Cog):
+    # this category works, but I can't get opus to work so this is pretty useless.
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # says when bot is online
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Currency cog is online.\n')
+        global amounts
+        try:
+            with open('amounts.json') as f:
+                amounts = json.load(f)
+        except FileNotFoundError:
+            print("Could not load amounts.json")
+            amounts = {}
+
+    @commands.command(pass_context=True, aliases=['bal'])
+    async def balance(self, ctx):
+        id = str(ctx.message.author.id)
+        if id in amounts:
+            await ctx.send("You have ${} in the bank".format(amounts[id]))
+            _save()
+        elif id not in amounts:
+            amounts[id] = 100
+            await ctx.send("You have ${} in the bank".format(amounts[id]))
+            _save()
+
+    @commands.command(pass_context=True)
+    async def register(self, ctx):
+        id = str(ctx.message.author.id)
+        if id not in amounts:
+            amounts[id] = 100
+            await ctx.send("You are now registered")
+            _save()
+        else:
+            await ctx.send("You already have an account")
+            _save()
+
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    @commands.command(pass_context=True)
+    async def work(self, ctx):
+        id = str(ctx.message.author.id)
+        income = int(random.randint(75, 150))
+        if id in amounts:
+            amounts[id] = amounts[id] + income
+            await ctx.send(f'You shook your ass on stage and got **${income}**. Your total is now **${amounts[id]}**.')
+            _save()
+        elif id not in amounts:
+            amounts[id] = 100 + income
+            await ctx.send(f'You shook your ass on stage and got **${income}**. Your total is now **${amounts[id]}**.')
+            _save()
+
+    @work.error
+    async def work_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            msg = 'This command is on cooldown for {:.2f} more seconds'.format(error.retry_after)
+            await ctx.send(msg)
+        else:
+            raise error
+
+    @commands.command(pass_context=True, aliases=['give', 'pay'])
+    async def transfer(self, ctx, amount: int, other: discord.Member):
+        primary_id = str(ctx.message.author.id)
+        other_id = str(other.id)
+        if primary_id not in amounts:
+            await ctx.send("You do not have an account")
+            _save()
+        elif other_id not in amounts:
+            await ctx.send("The other party does not have an account")
+            _save()
+        elif amounts[primary_id] < amount:
+            await ctx.send("You cannot afford this transaction")
+            _save()
+        else:
+            amounts[primary_id] -= amount
+            amounts[other_id] += amount
+            await ctx.send("Transaction complete")
+        _save()
+
+    @commands.is_owner()
+    @commands.command()
+    async def save(self):
+        _save()
+
+
+def setup(bot):
+    bot.add_cog(Currency(bot))
